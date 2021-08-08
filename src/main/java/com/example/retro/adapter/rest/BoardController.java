a30d01dfac1a5ecf52380cf6a245a318083c011b
Danny <mezenov.201521@gmail.com>
2021-08-09 01:06:13 +0300

    Реализация подключения к доске посредством веб-сокетов

---
+++ src/main/java/com/example/retro/adapter/rest/BoardController.java
@@ -0,0 +1,12 @@
+package com.example.retro.adapter.rest.dto;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+    GetMapping()
+
+}
